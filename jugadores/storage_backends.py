41656ad47efb9cdfa303b662964d1d1fdc90f8dc
Jeremy Fermín <jfermin@eurobuilding-hotels.com>
2026-09-18 20:32:39 -0400

    Corregir formato de storage_backends.py

--- jugadores/storage_backends.py
+++ jugadores/storage_backends.py
@@ -1,1 +1,9 @@
-import os from storages.backends.s3 import S3Storage class SupabasePublicStorage(S3Storage): def url(self, name, parameters=None, expire=None, http_method=None): base = os.environ.get('SUPABASE_PUBLIC_STORAGE_URL', '').rstrip('/') return f"{base}/{self.bucket_name}/{name}"
+import os
+
+from storages.backends.s3 import S3Storage
+
+
+class SupabasePublicStorage(S3Storage):
+    def url(self, name, parameters=None, expire=None, http_method=None):
+        base = os.environ.get('SUPABASE_PUBLIC_STORAGE_URL', '').rstrip('/')
+        return f"{base}/{self.bucket_name}/{name}"
